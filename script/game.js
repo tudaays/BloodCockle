8eeb33ad81ac20074beebff932bd1bdea835ac7b
thekeldeo <khanhktong@gmail.com>
2016-05-29 14:28:52 +0700

    .

--- script/game.js
+++ script/game.js
@@ -2,12 +2,14 @@
  * Created by keldeo on 29/05/2016.
  */
 var context;
+var mouseX, mouseY;
 window.onload = function () {
     var canvas = document.createElement("canvas");
     document.body.appendChild(canvas);
     context = canvas.getContext("2d");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    document.body.addEventListener("mousemove", mouseMove, false);
     gameStart();
     setInterval(gameLoop, 17);
 };
@@ -31,6 +33,23 @@ function gameDrawer() {
     context.fillRect(0, 0, window.innerWidth, window.innerHeight);
     player.draw(context);
 }
+
+function mouseMove(e)
+{
+
+    if(e.offsetX) {
+        mouseX = e.offsetX;
+        mouseY = e.offsetY;
+    }
+    else if(e.layerX) {
+        mouseX = e.layerX;
+        mouseY = e.layerY;
+    }
+
+
+    /* do something with mouseX/mouseY */
+}
+
 window.onkeydown = function (e) {
     switch (e.keyCode){
         case 65://a
@@ -45,9 +64,9 @@ window.onkeydown = function (e) {
         case 87://w
             player.move(1);
             break;
-        // case 32: // dau cach
-        //     player.shot();
-        //     break;
+        case 32: // dau cach
+            player.shoot();
+            break;
     }
 };
 window.onkeyup = function (e) {
--- script/tank.js
+++ script/tank.js
@@ -6,19 +6,25 @@ class Tank{
         this.x = x;
         this.y = y;
         this.sprite = new Image();
+        this.spriteUp = new Image();
         this.sprite.src = 'img/Base_tank.png';
         this.level = 1;
-        this.speedX = 3;
-        this.speedY = 3;
+        this.speedX = 0;
+        this.speedY = 0;
+        this.direction =1;
+        this.bullets = new Array();
     }
     update(){
         this.x += this.speedX;
         this.y += this.speedY;
     }
     draw(context){
-        context.drawImage(this.sprite, this.x, this.y);
+        context.save();
+        context.translate(this.x, this.y);
+        context.rotate(this.rotate(mouseX, mouseY));
+        context.drawImage(this.sprite, 0, 0);
+        context.restore();
     }
-    //direction o day khac voi dir cua nong sung
     move(direction){
         switch (direction){
             case 1://up
@@ -26,30 +32,44 @@ class Tank{
                     this.speedY = -4;
                     this.speedX = 0;
                 }
-                //this.direction = 1;
+                this.direction = 1;
                 break;
             case 2://down
                 if(this.y <= window.innerHeight - 10){
                     this.speedY = 4;
                     this.speedX = 0;
                 }
-                //this.direction = 2;
+                this.direction = 2;
                 break;
             case 3://left
                 if(this.x >= 10){
                     this.speedX = -4;
                     this.speedY = 0;
                 }
-                //this.direction = 3;
+                this.direction = 3;
                 break;
             case 4://right
                 if (this.x <= window.innerWidth -10){
                     this.speedX = 4;
                     this.speedY = 0;
                 }
-                //  this.direction = 4;
+                 this.direction = 4;
                 break;
         }
     }
+    rotate(x1, y1){
+        var c1= Math.abs(y1- this.y);
+        var c2 = Math.sqrt((x1-this.x)*(x1-this.x) + (y1-this.y)*(y1-this.y))
+        if(x1 > this.x && y1 >this.y){  // truong hop goc phan tu thu 4
+            var res = Math.asin(c1/c2);
+            return res;
+        }
+        if(this.x > x1 && y1> this.y){
+            var res = Math.PI - Math.asin(c1/c2);;
+        }
 
+    }
+    shoot(){
+        var bullet = new Bullet(this.x + 30, this.y+30, t);
+    }
 }
