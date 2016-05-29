da5002a410f5df5e1f0a86b53d3102121f550b60
thekeldeo <khanhktong@gmail.com>
2016-05-29 15:32:24 +0700

    update dan

--- script/bullet.js
+++ script/bullet.js
@@ -2,24 +2,32 @@
  * Created by kej on 29/05/2016.
  */
 class Bullet{
-    constructor(x, y, direction){
+    constructor(x, y, degree, direction){
         this.x = x;
         this.y = y;
-        this.damage = 0;
+        this.sprite = new Image();
+        this.sprite.src = 'img/bullet.png';
+        this.degree = degree;
         this.speedX = 0;
         this.speedY = 0;
+        this.direction = direction;
         this.reloadTime = 0;
-        switch (direction){
+        this.damage = 0;
+        switch (this.direction){
             case 1: //up
-                this.speedY = -5;
+                this.speedY = -5/Math.tan(degree);
+                this.speedX = 5;
                 break;
             case 2: //down
-                this.speedY = 5;
+                this.speedY = -5/Math.tan(degree);
+                this.speedX =-5;
                 break;
             case 3: //left
+                this.speedY = 5/Math.tan(degree);
                 this.speedX = -5;
                 break;
             case 4: //right
+                this.speedY = 5/Math.tan(degree);
                 this.speedX = 5;
                 break;
         }
@@ -30,6 +38,6 @@ class Bullet{
         this.y+=this.speedY;
     }
     draw(context){
-        context.drawImage(this.x, this.y);
+        context.drawImage(this.sprite,this.x, this.y);
     }
 }
--- script/tank.js
+++ script/tank.js
@@ -13,12 +13,20 @@ class Tank{
         this.speedY = 0;
         this.direction =1;
         this.bullets = new Array();
+        this.degree = 0;
+        this.direction = 0;
     }
     update(){
+        for(var i=0; i< this.bullets.length; i++){
+            this.bullets[i].update();
+        }
         this.x += this.speedX;
         this.y += this.speedY;
     }
     draw(context){
+        for(var i=0; i< this.bullets.length; i++){
+            this.bullets[i].draw(context);
+        }
         context.save();
         context.translate(this.x, this.y);
         context.rotate(this.rotate(mouseX, mouseY));
@@ -32,28 +40,24 @@ class Tank{
                     this.speedY = -4;
                     this.speedX = 0;
                 }
-                this.direction = 1;
                 break;
             case 2://down
                 if(this.y <= window.innerHeight - 10){
                     this.speedY = 4;
                     this.speedX = 0;
                 }
-                this.direction = 2;
                 break;
             case 3://left
                 if(this.x >= 10){
                     this.speedX = -4;
                     this.speedY = 0;
                 }
-                this.direction = 3;
                 break;
             case 4://right
                 if (this.x <= window.innerWidth -10){
                     this.speedX = 4;
                     this.speedY = 0;
                 }
-                 this.direction = 4;
                 break;
         }
     }
@@ -62,23 +66,32 @@ class Tank{
         var c2 = Math.sqrt((x1-this.x)*(x1-this.x) + (y1-this.y)*(y1-this.y))
         if(x1 >= this.x && y1 >= this.y){  // truong hop goc phan tu thu 4
             var res = Math.asin(c1/c2);
+            this.degree = res;
+            this.direction = 4;
             return res;
         }
         if(this.x >= x1 && y1>= this.y){
             var res = Math.PI - Math.asin(c1/c2);
+            this.degree = res;
+            this.direction = 3;
             return res;
         }
         if(this.x >= x1 && this.y>= y1 ){
             var res = Math.PI + Math.asin(c1/c2);
+            this.degree = res;
+            this.direction = 2;
             return res;
         }
         if(this.x <= x1 && this.y>= y1 ){
             var res = 2*Math.PI - Math.asin(c1/c2);
+            this.degree = res;
+            this.direction = 1;
             return res;
         }
 
     }
     shoot(){
-        var bullet = new Bullet(this.x + 30, this.y+30, t);
+        var bullet = new Bullet(this.x + 30, this.y+30, this.degree, this.direction);
+        this.bullets.push(bullet);
     }
 }
