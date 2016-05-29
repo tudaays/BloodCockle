e8fcccef78b01306071c1cfa458664691fe92ff7
thekeldeo <khanhktong@gmail.com>
2016-05-30 01:36:51 +0700

    em đã làm cho đạn di chuyển theo hướng của tank + làm thời gian delay đạn + có thêm một số comment

--- script/bullet.js
+++ script/bullet.js
@@ -2,37 +2,18 @@
  * Created by kej on 29/05/2016.
  */
 class Bullet{
-    constructor(x, y, degree, direction){
+    constructor(x, y, x1, y1){
         this.x = x;
         this.y = y;
         this.sprite = new Image();
         this.sprite.src = 'img/bullet.png';
-        this.degree = degree;
-        this.speedX = 0;
-        this.speedY = 0;
-        this.direction = direction;
-        this.reloadTime = 0;
+        this.speedX = (x1 - x)/10; //2 cái này đang lỗi em nghiên cứu sau
+        this.speedY = (y1 - y)/10;
         this.damage = 0;
     }
     update(){
-        switch (this.direction){
-            case 1: //up
-                this.y -= 5/Math.tan(this.degree);
-                this.x += 5;
-                break;
-            case 2: //down
-                this.y -= 5/Math.tan(this.degree);
-                this.x -= 5;
-                break;
-            case 3: //left
-                this.y += 5/Math.tan(this.degree);
-                this.x -= 5;
-                break;
-            case 4: //right
-                this.y += 5/Math.tan(this.degree);
-                this.x += 5;
-                break;
-        }
+        this.x += this.speedX;
+        this.y += this.speedY;
     }
     draw(context){
         context.drawImage(this.sprite,this.x, this.y);
--- script/tank.js
+++ script/tank.js
@@ -5,32 +5,36 @@ class Tank{
     constructor(x, y){
         this.x = x;
         this.y = y;
+        this.speedX = 0;
+        this.speedY = 0;
         this.sprite = new Image();
         this.spriteUp = new Image();
         this.sprite.src = 'img/Base_tank.png';
-        this.level = 1;
-        this.speedX = 0;
-        this.speedY = 0;
-        this.direction =1;
+        this.level = 1; //chưa làm
+        this.reload = 30; // thoi gian delay cua bullet 30x17ms = ..
+        this.speedBullet = 0; // chưa làm
         this.bullets = new Array();
-        this.degree = 0;
-        this.direction = 0;
+        this.degree = 0; // goc de xoay
+        this.count = 0; // biến đếm số lần lặp của gameLoop để tính time delay đạn
     }
     update(){
-        for(var i=0; i< this.bullets.length; i++){
+        this.count ++; // mỗi lần lặp gameLoop tăng biến đếm
+        for(var i=0; i< this.bullets.length; i++){ // update mảng các viên đạn
             this.bullets[i].update();
         }
-        this.x += this.speedX;
+        this.x += this.speedX; //update tank
         this.y += this.speedY;
     }
     draw(context){
         for(var i=0; i< this.bullets.length; i++){
             this.bullets[i].draw(context);
         }
+        //vẽ tank
+        //mouseX mouseY là tọa độ của mouse
         context.save();
         context.translate(this.x, this.y);
         context.rotate(this.rotate(mouseX, mouseY));
-        context.drawImage(this.sprite, -84, -80);
+        context.drawImage(this.sprite, -48, -48);
         context.restore();
     }
     move(direction){
@@ -61,37 +65,36 @@ class Tank{
                 break;
         }
     }
-    rotate(x1, y1){
+    rotate(x1, y1){ //no comment :(
         var c1= Math.abs(y1- this.y);
         var c2 = Math.sqrt((x1-this.x)*(x1-this.x) + (y1-this.y)*(y1-this.y))
         if(x1 >= this.x && y1 >= this.y){  // truong hop goc phan tu thu 4
             var res = Math.asin(c1/c2);
             this.degree = res;
-            this.direction = 4;
             return res;
         }
         if(this.x >= x1 && y1>= this.y){
             var res = Math.PI - Math.asin(c1/c2);
             this.degree = res;
-            this.direction = 3;
             return res;
         }
         if(this.x >= x1 && this.y>= y1 ){
             var res = Math.PI + Math.asin(c1/c2);
             this.degree = res;
-            this.direction = 2;
             return res;
         }
         if(this.x <= x1 && this.y>= y1 ){
             var res = 2*Math.PI - Math.asin(c1/c2);
             this.degree = res;
-            this.direction = 1;
             return res;
         }
 
     }
     shoot(){
-        var bullet = new Bullet(this.x, this.y, this.degree, this.direction);
-        this.bullets.push(bullet);
+        if(this.count >= this.reload){ //nếu count lớn hơn or bằng time delay mới cho nạp đạn vào. Sau khi nạp đạn thì set count =0
+            var bullet = new Bullet(this.x -20, this.y, mouseX, mouseY);
+            this.bullets.push(bullet);
+            this.count =0;
+        }
     }
 }
